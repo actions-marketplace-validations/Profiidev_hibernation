39ffc37df5db739c9fa1640079ec3a3283b3541b
GitHub Action <action@github.com>
2026-03-17 15:13:24 +0000

    Fix code style issues with rustfmt

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -15,14 +15,14 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
   fn migrations() -> Vec<Box<dyn MigrationTrait>> {
     vec![
-            Box::new(m20250301_215149_create_key_table::Migration),
-            Box::new(m20260123_144736_invalid_jwt::Migration),
-            Box::new(m20260123_144752_user::Migration),
-            Box::new(m20260126_155842_group::Migration),
-            Box::new(m20260126_160754_setup::Migration),
-            Box::new(m20260127_211643_settings::Migration),
-            Box::new(m20260129_154755_user_avatar::Migration),
-            Box::new(m20260317_135750_token::Migration),
-        ]
+      Box::new(m20250301_215149_create_key_table::Migration),
+      Box::new(m20260123_144736_invalid_jwt::Migration),
+      Box::new(m20260123_144752_user::Migration),
+      Box::new(m20260126_155842_group::Migration),
+      Box::new(m20260126_160754_setup::Migration),
+      Box::new(m20260127_211643_settings::Migration),
+      Box::new(m20260129_154755_user_avatar::Migration),
+      Box::new(m20260317_135750_token::Migration),
+    ]
   }
 }
