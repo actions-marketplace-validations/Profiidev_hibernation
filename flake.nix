fc35ff0e5d6661b0c22f889df8fd9bae01f5c4b3
ProfiDev <92174452+Profiidev@users.noreply.github.com>
2026-03-28 08:30:54 +0100

    Release version v0.1.2 (#19)
    * Updated version to v0.1.2
    
    * Fix code style issues with Prettier
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "hibernation";
-          version = "0.1.1";
+          version = "0.1.2";
 
           src = nix-filter {
             root = ./.;
